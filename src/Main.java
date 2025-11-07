a290af49ec6ea51eb0696be4c49c1f6b71bb0628
matia <matiassoegaard01@outlook.com>
2025-11-07 11:57:55 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Laver et objekt af SystemHandling; Viser menu, Giver valg muligheder for hvad du ville se, og håndtere listen af Leaders.
+        //Laver et objekt af SystemHandling;Som Giver valg muligheder for hvad du ville se, og håndtere listen af Leaders.
         SystemHandling systemHandling = new SystemHandling();
         //Kalder metoden handleSystem.
         systemHandling.handleSystem();
